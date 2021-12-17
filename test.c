b2ab761932c4f770e593f1c8bc41391255ad5aa1
z382196829 <1936851315@qq.com>
2021-12-17 23:32:27 +0800

    第二十八次提交

---
+++ test.c
@@ -0,0 +1,32 @@
+﻿#include<stdio.h>
+#include<windows.h>
+
+void main()
+{
+    int i = 0;//行
+    int j = 0;//星号个数
+    int k = 0;//空格数
+    int line = 0;//空心三角形的高
+    printf("请输入空心三角形的高：");
+    scanf("%d", &line);
+    for(i = 1; i <= line; i++)
+    {
+ 	for(k = 1; k <= line - i; k++)
+	{
+  	    printf(" ");
+	}
+	for(j = 1; j <= 2 * i - 1; j++)
+	{
+	    if(j == 1 || j == 2 * i - 1 || i == line)
+	    {
+		printf("*");
+	    }
+	    else
+	    {
+		printf(" ");
+  	    }
+	}
+	printf("\n");
+    }
+    system("pause");
+}
